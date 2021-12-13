15b243f2acd28024be125b955f5e71382a057499
Sriya Pathak <sriya.pathak@sriya298ub.successivetech.com>
2021-12-13 18:55:26 +0530

    header style

--- src/components/sidebarNav/styled.js
+++ src/components/sidebarNav/styled.js
@@ -2,12 +2,15 @@
 import styled from "styled-components";
 
 const SideNavWrapper = styled.div`
+// border: 1px solid;
+padding: 30px 15px;
 .sidenavmenu_list {
     // width: 15%;
     // margin: 0;
 }
 .sidenavmenu_list .nav-link{
     color: #333333;
+    font-weight: 500;
 }
 `;
 
